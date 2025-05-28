8749d69e4ef5a0ff7719e86bf0c4c91379f4ab49
BaeJungYeon <qowjdduswkd@naver.com>
2025-05-28 21:29:37 +0900

    feat : querydsl setting

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
   public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+    System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+    System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
     SpringApplication.run(DemoApplication.class, args);
   }
 }
