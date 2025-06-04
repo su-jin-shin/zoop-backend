158f9d42b8786722e6ec30093b354c5e45a20741
Jini-lab <chungyeojin828@gmail.com>
2025-06-04 17:10:56 +0900

    setting

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,23 +4,18 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-//public class DemoApplication {
-//  public static void main(String[] args) {
-//    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-//    System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
-//    System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
-//    SpringApplication.run(DemoApplication.class, args);
-//  }
-//}
-
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DemoApplication {
   public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+    System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+    System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+    System.setProperty("KAKAO_CLIENT_ID", dotenv.get("KAKAO_CLIENT_ID"));
+    System.setProperty("KAKAO_REDIRECT_URI", dotenv.get("KAKAO_REDIRECT_URI"));
+    System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+
     SpringApplication.run(DemoApplication.class, args);
   }
 }
+
+
