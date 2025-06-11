13ee0f6400d7a8f129381410a38c2d26504d0b8b
sejun0205 <sejun0205@gmail.com>
2025-06-12 02:06:05 +0900

    fix dto에서 list 문제에 대한 ci 에러 수정

--- src/main/java/com/example/demo/property/dto/PropertyFacilitiesResponseDto.java
+++ src/main/java/com/example/demo/property/dto/PropertyFacilitiesResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.demo.property.dto;
 
 import com.example.demo.property.domain.Property;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,10 @@ import lombok.NoArgsConstructor;
 import java.util.Collections;
 import java.util.List;
 
+@SuppressFBWarnings(
+        value = { "EI_EXPOSE_REP", "EI_EXPOSE_REP2" }
+
+)
 @Data
 @Builder
 @NoArgsConstructor
