4911a148c81961bb71f63472a19080c0b3e9cf51
sejun0205 <sejun0205@gmail.com>
2025-06-12 02:01:38 +0900

    feat 상세조회(시설정보) 기능

---
+++ src/main/java/com/example/demo/property/dto/PropertyFacilitiesResponseDto.java
@@ -0,0 +1,44 @@
+package com.example.demo.property.dto;
+
+import com.example.demo.property.domain.Property;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collections;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PropertyFacilitiesResponseDto {
+
+    private Long propertyId; //매물 아이디 (기본키)
+    private String heatMethodTypeName; //난방방식
+    private List<String> lifeFacilities; //생활시설
+    private List<String> securityFacilities; //보안시설
+    private List<String> etcFacilities; //기타시설
+
+
+    //빌더
+    public static  PropertyFacilitiesResponseDto of(Property p){
+        return  PropertyFacilitiesResponseDto.builder()
+                .propertyId(p.getPropertyId())
+                .heatMethodTypeName(p.getHeatMethodTypeName())
+                .lifeFacilities(safeList(p.getLifeFacilities()))
+                .securityFacilities(safeList(p.getSecurityFacilities()))
+                .etcFacilities(safeList(p.getEtcFacilities()))
+                .build();
+    }
+
+
+    //null 방지 메서드
+    private static List<String> safeList(List<String> list){
+        return  list == null ? Collections.emptyList() : list;
+    }
+
+
+
+}
