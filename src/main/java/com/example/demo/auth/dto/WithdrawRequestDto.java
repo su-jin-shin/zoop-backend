4f6f9a22c707080e24227d5c41448f618b3274ac
장교준 <wkddn2003@gmail.com>
2025-06-11 18:11:22 +0900

    fix: 회원탈퇴 수정

--- src/main/java/com/example/demo/auth/dto/WithdrawRequestDto.java
+++ src/main/java/com/example/demo/auth/dto/WithdrawRequestDto.java
@@ -2,10 +2,11 @@ package com.example.demo.auth.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class WithdrawRequestDto {
-
     private String withdrawReason;
 }
