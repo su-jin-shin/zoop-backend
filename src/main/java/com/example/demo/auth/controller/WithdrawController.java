223738d5a1284391c3323c48e846c0a5ce799d10
장교준 <wkddn2003@gmail.com>
2025-06-13 12:02:53 +0900

    fix: 로그인 예외처리 수정

--- src/main/java/com/example/demo/auth/controller/WithdrawController.java
+++ src/main/java/com/example/demo/auth/controller/WithdrawController.java
@@ -3,8 +3,11 @@ package com.example.demo.auth.controller;
 import com.example.demo.auth.dto.WithdrawRequestDto;
 import com.example.demo.auth.dto.LoginUser;
 import com.example.demo.auth.service.WithdrawServiceImpl;
+import com.example.demo.common.exception.UnauthorizedAccessException;
+import com.example.demo.common.response.ResponseResult;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -16,15 +19,23 @@ public class WithdrawController {
 
     private final WithdrawServiceImpl withdrawService;
 
-
-    // 탈퇴 + 사유 입력 (POST 방식)
-
     @PostMapping("/withdraw")
-    public ResponseEntity<String> withdraw(
+    public ResponseEntity<ResponseResult> withdraw(
             @AuthenticationPrincipal LoginUser loginUser,
             @Valid @RequestBody WithdrawRequestDto request) {
 
+        if (loginUser == null) {
+            throw new UnauthorizedAccessException(); // 403 처리
+        }
+
         withdrawService.withdraw(Long.valueOf(loginUser.getUsername()), request.getWithdrawReason());
-        return ResponseEntity.ok("탈퇴가 정상적으로 처리되었습니다.");
+
+        return ResponseEntity.ok(
+                ResponseResult.success(
+                        HttpStatus.OK,
+                        "탈퇴가 정상적으로 처리되었습니다.",
+                        null
+                )
+        );
     }
 }
