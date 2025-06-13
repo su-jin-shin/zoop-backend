51bd679b9c52929d5235fdd1f1ce2d57ccc3dc92
장교준 <wkddn2003@gmail.com>
2025-06-13 23:41:59 +0900

    fix: ci/cd 코드수정

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,13 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
   public static void main(String[] args) {
-    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-    System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-    System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-    System.setProperty("KAKAO_CLIENT_ID", dotenv.get("KAKAO_CLIENT_ID"));
-    System.setProperty("KAKAO_REDIRECT_URI", dotenv.get("KAKAO_REDIRECT_URI"));
-    System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
-    System.setProperty("SPRING_PROFILES_ACTIVE", dotenv.get("SPRING_PROFILES_ACTIVE"));
+    // 1) prod/prod-like 환경에선 건너뜀
+    if (System.getenv("SPRING_PROFILES_ACTIVE") == null
+            || "local".equals(System.getenv("SPRING_PROFILES_ACTIVE"))) {
+
+      // 2) .env 읽어 System properties 보강
+      Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+      dotenv.entries().forEach(e ->
+              System.setProperty(e.getKey(), e.getValue()));
+    }
 
     SpringApplication.run(DemoApplication.class, args);
   }
