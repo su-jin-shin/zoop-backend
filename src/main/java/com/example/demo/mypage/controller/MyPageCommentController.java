e27240d562c1e91fa7af76ed5863c0849e66d2b1
Jini-lab <chungyeojin828@gmail.com>
2025-06-20 15:49:54 +0900

    feat: mypage>delete myReview/myComment

--- src/main/java/com/example/demo/mypage/controller/MyPageCommentController.java
+++ src/main/java/com/example/demo/mypage/controller/MyPageCommentController.java
@@ -2,15 +2,18 @@ package com.example.demo.mypage.controller;
 
 import com.example.demo.auth.dto.LoginUser;
 import com.example.demo.common.exception.UserNotFoundException;
+import com.example.demo.common.response.FailedMessage;
+import com.example.demo.common.response.ResponseResult;
+import com.example.demo.common.response.SuccessMessage;
 import com.example.demo.mypage.dto.MyCommentResponse;
 import com.example.demo.mypage.service.MyCommentService;
+import com.example.demo.review.service.ReviewCommentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +23,8 @@ import java.util.List;
 @RequestMapping("/mypage")
 public class MyPageCommentController {
 
-    private final MyCommentService commentService;
+    private final MyCommentService myCommentService;
+    private final ReviewCommentService commentService;
 
     private Long parseUserId(LoginUser loginUser) {
         try {
@@ -38,8 +42,26 @@ public class MyPageCommentController {
             throw new UserNotFoundException();
         }
         log.info("코멘트 컨트롤러 시작. 유저id: " + userId);
-        List<MyCommentResponse> comments = commentService.getMyComments(userId);
+        List<MyCommentResponse> comments = myCommentService.getMyComments(userId);
 
         return ResponseEntity.ok(comments);
+    }
+
+    @DeleteMapping("/comments/{commentId}")
+    public ResponseEntity<?> deleteMyReview(@AuthenticationPrincipal LoginUser loginUser,
+                                            @PathVariable Long commentId) {
+
+        Long userId = parseUserId(loginUser);
+        if (userId == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(ResponseResult.failed(HttpStatus.UNAUTHORIZED, FailedMessage.LOGIN_REQUIRED.getMessage(), null));
+        }
+
+        commentService.deleteComment(commentId, userId);
+
+        return ResponseEntity.ok(ResponseResult.success(
+                HttpStatus.OK,
+                SuccessMessage.COMMENT_DELETED.getMessage(),
+                null));
     }
 }
