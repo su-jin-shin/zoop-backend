0126c58e6e67425d16594f5beaf6e15d08a68632
Jini-lab <chungyeojin828@gmail.com>
2025-06-21 18:13:34 +0900

    fix: mypage map and excel export

--- src/main/java/com/example/demo/common/excel/ExcelListResponse.java
+++ src/main/java/com/example/demo/common/excel/ExcelListResponse.java
@@ -4,14 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
-@Builder
+@Builder(toBuilder = true)
 public class ExcelListResponse<T> {
 
     private int countProperties;
-    private List<T> data;
+    private final List<T> data;
 
+    public ExcelListResponse(int countProperties, List<T> data) {
+        this.countProperties = countProperties;
+        this.data = data == null ? List.of() : new ArrayList<>(data);
+    }
+
+    public List<T> getData() {
+        return data == null ? null : new ArrayList<>(data);
+    }
+
+    public static class ExcelListResponseBuilder<T> {
+        public ExcelListResponseBuilder<T> data(List<T> data) {
+            this.data = data == null ? null : new ArrayList<>(data);
+            return this;
+        }
+    }
 }
+
